e5fbd106166b6a6cfc514a27a1d252fa91acb23e
Jack Francis <jack@deis.com>
2016-10-05 11:50:11 -0700

    chore(codecov): using local test-cover.sh to exclude swagger-generated code from coverage report

---
+++ src/api/_scripts/test-cover.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# shellcheck disable=SC2046
+pkgs=$(go list $(glide novendor))
+COVERAGE_EXCLUDES=("github.com/helm/monocular/src/api/pkg/swagger/models" \
+"github.com/helm/monocular/src/api/pkg/swagger/restapi" \
+"github.com/helm/monocular/src/api/pkg/swagger/restapi/operations" \
+"github.com/helm/monocular/src/api")
+
+echo "" > coverage.txt
+for p in $pkgs; do
+    if [[ " ${COVERAGE_EXCLUDES[@]} " =~ " ${p} " ]]; then
+      echo "skipping test coverage for ${p}"
+    else
+      go test -covermode=atomic -coverprofile=profile.out "$p"
+      if [ -s profile.out ]; then
+          cat profile.out >> coverage.txt
+      fi
+      rm -f profile.out
+    fi
+done
