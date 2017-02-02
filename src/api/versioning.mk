d32e5c6b61b0dd00ca526b9af06b73b1528f4b4b
Adnan Abdulhussein <adnan@bitnami.com>
2017-02-01 17:55:55 -0800

    feat(*): add helm chart

--- src/api/versioning.mk
+++ src/api/versioning.mk
@@ -1,10 +1,11 @@
 MUTABLE_VERSION ?= canary
 VERSION ?= git-$(shell git rev-parse --short HEAD)
-IMAGE_REGISTRY ?= '' # default to dockerhub
-IMAGE_PREFIX ?= '' # we rely upon the user providing an IMAGE_PREFIX, e.g., IMAGE_PREFIX=jackfrancis make docker-push
+IMAGE_REGISTRY ?= # default to dockerhub
+IMAGE_PREFIX ?= # we rely upon the user providing an IMAGE_PREFIX, e.g., IMAGE_PREFIX=jackfrancis make docker-push
+IMAGE_NAME ?= ${SHORT_NAME}-api
 
-IMAGE := ${IMAGE_REGISTRY_PREFIX}${IMAGE_PREFIX}/${SHORT_NAME}:${VERSION}
-MUTABLE_IMAGE := ${IMAGE_REGISTRY_PREFIX}${IMAGE_PREFIX}/${SHORT_NAME}:${MUTABLE_VERSION}
+IMAGE := ${IMAGE_REGISTRY}${IMAGE_PREFIX}/${IMAGE_NAME}:${VERSION}
+MUTABLE_IMAGE := ${IMAGE_REGISTRY}${IMAGE_PREFIX}/${IMAGE_NAME}:${MUTABLE_VERSION}
 
 .PHONY: docker-push
 docker-push: docker-immutable-push docker-mutable-push
