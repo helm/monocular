fd2c5fc319dcfbcbd9ceb2081c3b69518be8b2a5
Angel M De Miguel <angel@bitnami.com>
2017-02-13 11:27:52 +0100

    Set loader component as a wrapper

--- src/ui/src/app/loader/loader.component.ts
+++ src/ui/src/app/loader/loader.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  styleUrls: ['./loader.component.scss'],
+  inputs: ['loading']
 })
 export class LoaderComponent {
+  // Show the loader or the content
+  public loading: boolean = false;
+
   constructor() {}
 }
