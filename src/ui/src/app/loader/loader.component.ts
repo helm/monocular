7998816db8b2a3c5f117bbbbe008dea75109f866
Angel M De Miguel <angel@bitnami.com>
2017-02-13 11:12:40 +0100

    Create a loader component

---
+++ src/ui/src/app/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent {
+  constructor() {}
+}
