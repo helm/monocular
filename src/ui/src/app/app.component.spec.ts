eb730f74468f2d4a1f91587f74d21e7fba153fa7
Miguel Martinez <miguel@bitnami.com>
2017-02-15 17:03:51 -0800

    Fix test

--- src/ui/src/app/app.component.spec.ts
+++ src/ui/src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
