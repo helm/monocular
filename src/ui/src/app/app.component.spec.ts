c7f2676d07e82a39f792a25bc184c817f5658603
Miguel Martinez <miguel@bitnami.com>
2017-02-15 17:48:05 -0800

    Fix test

--- src/ui/src/app/app.component.spec.ts
+++ src/ui/src/app/app.component.spec.ts
@@ -3,11 +3,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { Angulartics2GoogleAnalytics } from 'angulartics2';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { FooterListComponent } from './footer-list/footer-list.component';
+
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -18,6 +19,9 @@ describe('AppComponent', () => {
       declarations: [ AppComponent, FooterComponent, FooterListComponent ],
       imports: [
         RouterTestingModule
+      ],
+      providers: [
+        { provide: Angulartics2GoogleAnalytics }
       ]
     })
     .compileComponents();
