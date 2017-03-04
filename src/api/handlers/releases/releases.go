e4fddf887978acca587f8758ef0ac028d10eb2b1
Miguel Martinez <miguel@bitnami.com>
2017-03-04 15:47:27 -0800

    Retrieve releases

--- src/api/handlers/releases/releases.go
+++ src/api/handlers/releases/releases.go
@@ -1,16 +1,17 @@
 package releases
 
 import (
-	"fmt"
 	"net/http"
 
 	middleware "github.com/go-openapi/runtime/middleware"
-	"github.com/helm/monocular/src/api/swagger/models"
-	releasesapi "github.com/helm/monocular/src/api/swagger/restapi/operations/releases"
-
 	helmclient "github.com/helm/monocular/src/api/data/helm/client"
 	helmreleases "github.com/helm/monocular/src/api/data/helm/releases"
 	"github.com/helm/monocular/src/api/data/helpers"
+	"github.com/helm/monocular/src/api/handlers"
+	"github.com/helm/monocular/src/api/swagger/models"
+	releasesapi "github.com/helm/monocular/src/api/swagger/restapi/operations/releases"
+	hapi_release5 "k8s.io/helm/pkg/proto/hapi/release"
+	rls "k8s.io/helm/pkg/proto/hapi/services"
 )
 
 // GetReleases returns all the existing releases in your cluster
@@ -23,8 +24,10 @@ func GetReleases(params releasesapi.GetAllReleasesParams) middleware.Responder {
 	if err != nil {
 		return error("Error retrieving the list of releases")
 	}
-	fmt.Printf("%+v", releases)
-	return releasesapi.NewGetAllReleasesOK()
+
+	resources := makeReleaseResources(releases)
+	payload := handlers.DataResourcesBody(resources)
+	return releasesapi.NewGetAllReleasesOK().WithPayload(payload)
 }
 
 // CreateRelease installs a chart version
@@ -32,10 +35,31 @@ func CreateRelease(params releasesapi.CreateReleaseParams) middleware.Responder 
 	return releasesapi.NewCreateReleaseCreated()
 }
 
-// error is a convenience that contains a swagger-friendly 500 given a resource string
-func error(resource string) middleware.Responder {
-	message := fmt.Sprintf("500 %s not found", resource)
-	return releasesapi.NewGetAllReleasesDefault(http.StatusExpectationFailed).WithPayload(
-		&models.Error{Code: helpers.Int64ToPtr(http.StatusExpectationFailed), Message: &message},
+// error is a convenience that contains a swagger-friendly 500 given a string
+func error(message string) middleware.Responder {
+	return releasesapi.NewGetAllReleasesDefault(http.StatusInternalServerError).WithPayload(
+		&models.Error{Code: helpers.Int64ToPtr(http.StatusInternalServerError), Message: &message},
 	)
+}
+
+func makeReleaseResources(releases *rls.ListReleasesResponse) []*models.Resource {
+	var resources []*models.Resource
+	for _, release := range releases.Releases {
+		resource := makeReleaseResource(release)
+		resources = append(resources, resource)
+	}
+	return resources
+}
+
+func makeReleaseResource(release *hapi_release5.Release) *models.Resource {
+	var ret models.Resource
+	ret.Type = helpers.StrToPtr("release")
+	ret.ID = helpers.StrToPtr(release.Name)
+	ret.Attributes = &models.Release{
+		Chart:     &release.Chart.Metadata.Name,
+		Name:      &release.Name,
+		Namespace: &release.Namespace,
+		Status:    helpers.StrToPtr(release.Info.Status.Code.String()),
+	}
+	return &ret
 }
