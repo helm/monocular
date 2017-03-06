49c87c259d063dcf2ed5cb1687064fe6dad77ca7
Miguel Martinez <miguel@bitnami.com>
2017-03-05 22:08:52 -0800

    Move refresh to foreground

--- src/api/jobs/jobs_test.go
+++ src/api/jobs/jobs_test.go
@@ -9,11 +9,12 @@ import (
 )
 
 type testPeriodic struct {
-	t    *testing.T
-	err  error
-	i    int
-	freq time.Duration
-	name string
+	t        *testing.T
+	err      error
+	i        int
+	freq     time.Duration
+	name     string
+	firstRun bool
 }
 
 func (t *testPeriodic) Do() error {
@@ -30,9 +31,13 @@ func (t testPeriodic) Name() string {
 	return t.name
 }
 
+func (t testPeriodic) FirstRun() bool {
+	return t.firstRun
+}
+
 func TestDoPeriodic(t *testing.T) {
-	interval := time.Duration(3000) * time.Millisecond
-	p := &testPeriodic{t: t, err: nil, freq: interval, name: "test-periodic-job"}
+	interval := time.Duration(500) * time.Millisecond
+	p := &testPeriodic{t: t, err: nil, freq: interval, name: "test-periodic-job", firstRun: true}
 	canceller := DoPeriodic([]Periodic{p})
 	time.Sleep(interval / 2) // wait a little while for the goroutine to call the job once
 	assert.True(t, p.i == 1, "the periodic wasn't called once")
@@ -44,12 +49,23 @@ func TestDoPeriodic(t *testing.T) {
 }
 
 func TestDoPeriodicWithError(t *testing.T) {
-	interval := time.Duration(3000) * time.Millisecond
-	p := &testPeriodic{t: t, err: errors.New("Do() crashes"), freq: interval, name: "test-periodic-job"}
+	interval := time.Duration(500) * time.Millisecond
+	p := &testPeriodic{t: t, err: errors.New("Do() crashes"), freq: interval, name: "test-periodic-job", firstRun: true}
 	canceller := DoPeriodic([]Periodic{p})
 	time.Sleep(interval / 2) // wait a little while for the goroutine to call the job once
 	assert.True(t, p.i == 1, "the periodic wasn't called once")
 	time.Sleep(interval)
 	assert.True(t, p.i == 2, "the periodic wasn't called twice")
+	canceller()
+}
+
+func TestDoPeriodicNoFirstRun(t *testing.T) {
+	interval := time.Duration(500) * time.Millisecond
+	p := &testPeriodic{t: t, err: nil, freq: interval, name: "test-periodic-job2", firstRun: false}
+	canceller := DoPeriodic([]Periodic{p})
+	time.Sleep(interval / 2) // wait a little while for the goroutine to call the job once
+	assert.Equal(t, p.i, 0, "the periodic has not being called")
+	time.Sleep(interval)
+	assert.Equal(t, p.i, 1, "the periodic has being called once")
 	canceller()
 }
