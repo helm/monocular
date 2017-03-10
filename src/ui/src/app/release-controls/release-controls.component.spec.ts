f1991397a04a6addfb765341a140b280a955ede9
Miguel Martinez <miguel@bitnami.com>
2017-03-10 15:24:30 -0800

    Adding back delete releases from list

---
+++ src/ui/src/app/release-controls/release-controls.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ReleaseControlsComponent } from './release-controls.component';
+
+describe('Component: ReleaseControls', () => {
+  it('should create an instance', () => {
+    let component = new ReleaseControlsComponent();
+    expect(component).toBeTruthy();
+  });
+});
