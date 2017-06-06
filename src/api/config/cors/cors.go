4001897b6fb3e07b01106646b4c45c1ff1c53631
Adnan Abdulhussein <adnan@bitnami.com>
2017-06-06 17:41:53 +0200

    enable configuring CORS to remove same-domain requirement
    fixes #235

--- src/api/config/cors/cors.go
+++ src/api/config/cors/cors.go
@@ -1,21 +1,25 @@
 package cors
 
-import "os"
+import (
+	"io/ioutil"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type corsYAML struct {
+	Cors Cors
+}
 
 // Cors configuration used during middleware setup
 type Cors struct {
-	AllowedOrigins []string
-	AllowedHeaders []string
+	AllowedOrigins []string `yaml:"allowed_origins"`
+	AllowedHeaders []string `yaml:"allowed_headers"`
 }
 
-var currentEnv = func() string {
-	return os.Getenv("ENVIRONMENT")
-}
-
-// Config returns the CORS configuration for the environment
-// TODO, read the configuration from the overrides config file argument
-func Config(configFile string) (Cors, error) {
-	env := currentEnv()
+func defaultCors() (Cors, error) {
+	env := os.Getenv("ENVIRONMENT")
 	if env == "development" {
 		return Cors{
 			AllowedOrigins: []string{"*"},
@@ -26,4 +30,37 @@ func Config(configFile string) (Cors, error) {
 		AllowedOrigins: []string{"my-api-server"},
 		AllowedHeaders: []string{"access-control-allow-headers", "x-xsrf-token"},
 	}, nil
+}
+
+// Config returns the CORS configuration for the environment
+func Config(configFile string) (Cors, error) {
+	_, err := os.Stat(configFile)
+	if os.IsNotExist(err) {
+		log.Info("Loading default repositories")
+		return defaultCors()
+	}
+
+	log.Info("Loading repositories from config file")
+	cors, err := loadCorsFromFile(configFile)
+	if err != nil {
+		return Cors{}, err
+	}
+
+	if len(cors.AllowedOrigins) == 0 && len(cors.AllowedHeaders) == 0 {
+		return defaultCors()
+	}
+
+	return cors, nil
+}
+
+func loadCorsFromFile(filePath string) (Cors, error) {
+	var yamlStruct corsYAML
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return Cors{}, err
+	}
+	if err := yaml.Unmarshal(bytes, &yamlStruct); err != nil {
+		return Cors{}, err
+	}
+	return yamlStruct.Cors, nil
 }
