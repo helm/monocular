2b661b5758a705c731c9741661d0b313f4c3bfc7
Adnan Abdulhussein <adnan@bitnami.com>
2017-06-15 17:25:14 +0200

    build docker images

--- src/api/versioning.mk
+++ src/api/versioning.mk
@@ -1,19 +1,8 @@
-MUTABLE_VERSION ?= canary
-VERSION ?= git-$(shell git rev-parse --short HEAD)
-IMAGE_REGISTRY ?= # default to dockerhub
-IMAGE_PREFIX ?= # we rely upon the user providing an IMAGE_PREFIX, e.g., IMAGE_PREFIX=jackfrancis make docker-push
-IMAGE_NAME ?= ${SHORT_NAME}-api
+IMAGE_REPO ?= bitnami/${SHORT_NAME}-api
+IMAGE_TAG ?= latest
 
-IMAGE := ${IMAGE_REGISTRY}${IMAGE_PREFIX}/${IMAGE_NAME}:${VERSION}
-MUTABLE_IMAGE := ${IMAGE_REGISTRY}${IMAGE_PREFIX}/${IMAGE_NAME}:${MUTABLE_VERSION}
+IMAGE := ${IMAGE_REPO}:${IMAGE_TAG}
 
 .PHONY: docker-push
-docker-push: docker-immutable-push docker-mutable-push
-
-.PHONY: docker-immutable-push
-docker-immutable-push:
+docker-push:
 	docker push ${IMAGE}
-
-.PHONY: docker-mutable-push
-docker-mutable-push:
-	docker push ${MUTABLE_IMAGE}
