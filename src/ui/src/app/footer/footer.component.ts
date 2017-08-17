9c9544a776ef48bd62f56369ca5eee159c0a4d5a
Adnan Abdulhussein <adnan@prydoni.us>
2017-08-17 12:02:09 +0100

    show monocular version in ui (#337)
    * show monocular version in ui
    
    * improve VERSION default in Makefile
    
    * make monocularVersion field in FooterComponent public

--- src/ui/src/app/footer/footer.component.ts
+++ src/ui/src/app/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MONOCULAR_VERSION } from '../../version';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent { }
+export class FooterComponent {
+  monocularVersion: string = MONOCULAR_VERSION;
+}
---
+++ src/ui/src/version.ts
@@ -0,0 +1,2 @@
+// This version is automatically set during the release process, see `make set-version`
+export const MONOCULAR_VERSION = 'dev';
