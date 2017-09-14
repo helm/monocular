1a9ec140b5afb0dc6656edb07ae866f9d4eef61f
Adnan Abdulhussein <adnan@bitnami.com>
2017-09-14 16:37:41 +0100

    add Dockerfile for angular dev

---
+++ dev_env/ui/rootfs/app-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+. /opt/bitnami/base/functions
+
+INIT_SEM=/tmp/initialized.sem
+PACKAGE_FILE=/app/package.json
+
+fresh_container() {
+  [ ! -f $INIT_SEM ]
+}
+
+dependencies_up_to_date() {
+  # It is up to date if the package file is older than
+  # the last time the container was initialized
+  [ ! $PACKAGE_FILE -nt $INIT_SEM ]
+}
+
+if [ "$1" == ng -a "$2" == "serve" ]; then
+  if ! dependencies_up_to_date; then
+    log "Installing/Updating Angular dependencies (npm)"
+    yarn
+    log "Dependencies updated"
+  fi
+
+  if ! fresh_container; then
+    echo "#########################################################################"
+    echo "                                                                       "
+    echo " App initialization skipped:"
+    echo " Delete the file $INIT_SEM and restart the container to reinitialize"
+    echo " You can alternatively run specific commands using docker-compose exec"
+    echo " e.g docker-compose exec myapp npm install angular"
+    echo "                                                                       "
+    echo "#########################################################################"
+  else
+    log "Initialization finished"
+  fi
+
+  touch $INIT_SEM
+fi
+
+exec tini -- "$@"
