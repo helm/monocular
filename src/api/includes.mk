ec1500bd9d91327c90d38a96a415a3c161d6eb93
Adnan Abdulhussein <adnan@prydoni.us>
2018-09-19 16:39:21 -0700

    remove existing Monocular API implementation (#518)
    * remove src/api
    
    Signed-off-by: Adnan Abdulhussein <adnan@bitnami.com>
    
    * remove dev environment for monocular API
    
    Signed-off-by: Adnan Abdulhussein <adnan@bitnami.com>
    
    * remove gitignores related to the monocular API
    
    Signed-off-by: Adnan Abdulhussein <adnan@bitnami.com>
    
    * remove API-related CI tasks
    
    Signed-off-by: Adnan Abdulhussein <adnan@bitnami.com>

--- src/api/includes.mk
+++
@@ -1,8 +0,0 @@
-define check-static-binary
-	  if file $(1) | egrep -q "(statically linked|Mach-O)"; then \
-	    echo ""; \
-	  else \
-	    echo "The binary file $(1) is not statically linked. Build canceled"; \
-	    exit 1; \
-	  fi
-endef
